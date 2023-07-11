3eae33443f183a36812509f775683dc439b7588e
FeliXL2111 <febernrad@web.de>
2023-07-11 13:50:35 +0200

    page better and login

---
+++ learnsystem/app/login.tsx
@@ -0,0 +1,10 @@
+export default function Home(){
+    return (
+        <main>
+            <div id="maincard">
+                <input type="text" id="email"/>
+                <input type="text" id="password" />
+            </div>
+        </main>
+    )
+}
